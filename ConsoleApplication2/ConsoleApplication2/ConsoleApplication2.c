addf72c0c1adfbc43d0f9b51c8ef9e739bb7dc26
rongirl <gro.pretty.arabella@mail.ru>
2021-10-12 15:04:10 +0300

    Initial commit

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    int value;
+    struct QueueElement* next;
+}QueueElement;
+
+typedef struct 
+{
+    struct QueueElement* head;
+    struct QueueElement* tail;
+}Queue; 
+
+
+void enqueue()
+{
+
+}
+
+int dequeue()
+{
+
+}
+
+bool isEmpty()
+{
+
+}
+
+void delete()
+{
+
+}
+
+Queue* create()
+{
+     Queue* queue = calloc(1, sizeof(Queue);
+}
+
+int main()
+{
+
+}
